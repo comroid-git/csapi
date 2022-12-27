33778e45c99703cad5f4d3589c3910230b28c822
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-27 05:27:30 +0100

    Initial commit

---
+++ common/AnsiUtil.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace comroid.csapi.common
+    // ReSharper disable once ArrangeNamespaceBody
+{
+// https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+    public class AnsiUtil
+    {
+        public const string Reset = "\u001b[0m";
+        public const string Black = "\u001b[30m";
+        public const string Red = "\u001b[31m";
+        public const string Green = "\u001b[32m";
+        public const string Yellow = "\u001b[33m";
+        public const string Blue = "\u001b[34m";
+        public const string Magenta = "\u001b[35m";
+        public const string Cyan = "\u001b[36m";
+        public const string White = "\u001b[37m";
+        public const string BrightBlack = "\u001b[30;1m";
+        public const string BrightRed = "\u001b[31;1m";
+        public const string BrightGreen = "\u001b[32;1m";
+        public const string BrightYellow = "\u001b[33;1m";
+        public const string BrightBlue = "\u001b[34;1m";
+        public const string BrightMagenta = "\u001b[35;1m";
+        public const string BrightCyan = "\u001b[36;1m";
+        public const string BrightWhite = "\u001b[37;1m";
+        public const string BackgroundBlack = "\u001b[40m";
+        public const string BackgroundRed = "\u001b[41m";
+        public const string BackgroundGreen = "\u001b[42m";
+        public const string BackgroundYellow = "\u001b[43m";
+        public const string BackgroundBlue = "\u001b[44m";
+        public const string BackgroundMagenta = "\u001b[45m";
+        public const string BackgroundCyan = "\u001b[46m";
+        public const string BackgroundWhite = "\u001b[47m";
+        public const string BackgroundBrightBlack = "\u001b[40;1m";
+        public const string BackgroundBrightRed = "\u001b[41;1m";
+        public const string BackgroundBrightGreen = "\u001b[42;1m";
+        public const string BackgroundBrightYellow = "\u001b[43;1m";
+        public const string BackgroundBrightBlue = "\u001b[44;1m";
+        public const string BackgroundBrightMagenta = "\u001b[45;1m";
+        public const string BackgroundBrightCyan = "\u001b[46;1m";
+        public const string BackgroundBrightWhite = "\u001b[47;1m";
+        public const string Bold = "\u001b[1m";
+        public const string Underline = "\u001b[4m";
+        public const string Reversed = "\u001b[7m";
+        public const string CursorUp = "\u001b[{1}A";
+        public const string CursorDown = "\u001b[{1}B";
+        public const string CursorRight = "\u001b[{1}C";
+        public const string CursorLeft = "\u001b[{1}D";
+        public const string ClearScreen = "\u001b[{2}J";
+        public const string ClearLine = "\u001b[{2}K";
+
+        private const int STD_OUTPUT_HANDLE = -11;
+        private const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
+        private const uint DISABLE_NEWLINE_AUTO_RETURN = 0x0008;
+
+        [DllImport("kernel32.dll")]
+        private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetLastError();
+
+        public static string ByteColor(byte b) => $"\u001b[38;5;${b}m";
+
+        public static void Init()
+        {
+            var iStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+            if (!GetConsoleMode(iStdOut, out uint outConsoleMode))
+            {
+                Console.WriteLine("Failed to get output console mode");
+                Console.ReadKey();
+                return;
+            }
+
+            outConsoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
+            if (!SetConsoleMode(iStdOut, outConsoleMode))
+            {
+                Console.WriteLine($"Failed to set output console mode, error code: {GetLastError()}");
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
---
+++ common/StringUtil.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace comroid.csapi.common
+    // ReSharper disable once ArrangeNamespaceBody
+{
+    public static class StringUtil
+    {
+        public static string Adjust(this string str, int len, bool rightBound = false, bool doFill = true,
+            char fill = ' ')
+        {
+            str = str.Trim();
+            var n = len - str.Length;
+            if (n < 0) // remove n chars
+                if (rightBound)
+                    str = str.Substring(0, len);
+                else str = str.Substring(Math.Abs(n), str.Length + n);
+            else if (n > 0 && doFill)
+            {
+                // pre-/append n chars
+                var extra = string.Empty;
+                for (int i = 0; i < n; i++)
+                    extra += fill;
+                if (rightBound)
+                    str = extra + str;
+                else str += extra;
+            }
+
+            return str;
+        }
+    }
+
+    [SuppressMessage("ReSharper", "ArrangeObjectCreationWhenTypeEvident")]
+    public class TextTable
+    {
+        public readonly List<Column> Columns = new List<Column>();
+        public readonly List<Row> Rows = new List<Row>();
+        private readonly bool _header;
+        private readonly LineMode? _lineMode;
+        private const int margin = 1;
+
+        public TextTable(bool header = true, LineMode? lineMode = null)
+        {
+            _header = header;
+            _lineMode = lineMode;
+        }
+
+        public Column AddColumn(string name, bool justifyRight = false)
+        {
+            var col = new Column(name, justifyRight);
+            Columns.Add(col);
+            return col;
+        }
+
+        public Row AddRow()
+        {
+            var row = new Row();
+            foreach (var col in Columns)
+                row._data[col] = string.Empty;
+            Rows.Add(row);
+            return row;
+        }
+
+        public void AddSeparator() => Rows.Add(new SeparatorRow());
+
+        public override string ToString()
+        {
+            var cc = Columns.Count;
+            var lens = new int[cc];
+            for (var i = 0; i < cc; i++)
+            {
+                // for each column, collect longest data
+                var col = Columns[i];
+                foreach (var data in new[] { _header ? col.Name : string.Empty }
+                             .Concat(Rows
+                                 .Where(row => !row.Separator)
+                                 .Select(row => row._data[col])))
+                {
+                    var len = data.ToString()!.Length;
+                    if (lens[i] < len)
+                        lens[i] = len;
+                }
+            }
+
+            // todo: include outlines & inlines
+            var sb = new StringBuilder();
+            var lines = _lineMode != null;
+            var totalW = lens.Aggregate(0, (a, b) => a + b + margin/**/) + cc + lens.Length * margin;
+            var colTrims = lens.Select(x => 1 + x + margin * 2).Append(0).ToArray();
+            if (_header)
+            {
+                if (lines) sb.Append(HoriDetailLine(totalW, colTrims, LineType.ConD, LineType.Bold));
+
+                var dir = LineType.ConR;
+                for (var i = 0; i < cc; i++)
+                {
+                    if (i == cc)
+                        dir &= ~LineType.ConR;
+                    if (lines)
+                        sb.Append(VertIndent(dir, LineType.Bold));
+                    dir |= LineType.ConL;
+                    sb.Append(Columns[i].Name.Adjust(lens[i]));
+                    if (!lines)
+                        sb.Append(' ');
+                }
+                if (lines)
+                    sb.Append(VertIndent(dir, LineType.Bold));
+                sb.AppendLine();
+            }
+
+            if (lines)
+                sb.Append(HoriDetailLine(totalW, colTrims,
+                        _header ? LineType.IdxVertical : LineType.ConD,
+                        _header ? LineType.Bold : LineType.None));
+            for (var ri = 0; ri < Rows.Count; ri++)
+            {
+                var row = Rows[ri];
+                if (lines && row.Separator)
+                {
+                    sb.Append(HoriDetailLine(totalW, colTrims, LineType.IdxVertical));
+                    continue;
+                }
+                var dir = LineType.ConR;
+                for (var i = 0; i < cc; i++)
+                {
+                    var col = Columns[i];
+                    if (i == cc)
+                        dir &= ~LineType.ConR;
+                    if (lines)
+                        sb.Append(VertIndent(dir));
+                    dir |= LineType.ConL;
+                    sb.Append(row._data[col].ToString()!.Adjust(lens[i], col._justifyRight));
+                    if (!lines)
+                        sb.Append(' ');
+                }
+                if (lines)
+                    sb.Append(VertIndent(LineType.ConL));
+
+                sb.AppendLine();
+            }
+
+            if (lines) sb.Append(HoriDetailLine(totalW, colTrims, LineType.ConU));
+
+            return sb.ToString();
+        }
+
+        public class Row
+        {
+            internal readonly Dictionary<Column, object> _data = new Dictionary<Column, object>();
+
+            protected internal virtual bool Separator => false;
+
+            public Row SetData(Column col, object data)
+            {
+                _data[col] = data;
+                return this;
+            }
+        }
+
+        private class SeparatorRow : Row
+        {
+            protected internal override bool Separator => true;
+        }
+
+        public class Column
+        {
+            public readonly string Name;
+            internal readonly bool _justifyRight;
+
+            public Column(string name, bool justifyRight)
+            {
+                Name = name;
+                _justifyRight = justifyRight;
+            }
+        }
+
+        #region Table Lining
+        private char GetLining(LineType type)
+        {
+            var db = LiningSymbols[_lineMode ?? LineMode.ASCII];
+            if (db.Keys.Contains(type))
+                return db[type];
+            (int nh, char c)? best = null;
+            foreach (var (id, c) in db)
+            {
+                var comp = type & id & ~LineType.Bold;
+                var bits = Convert.ToString((int)comp, 2).Count(x => x == '1');
+                if (comp == (type & ~LineType.Bold))
+                    return c;
+                if (bits >= (best?.nh ?? 0))
+                    best = (bits, c);
+            }
+            return best?.c ?? '#';
+        }
+
+        private string VertIndent(LineType dir, LineType detail = LineType.None)
+        {
+            var str = string.Empty;
+            if ((dir & LineType.ConL) != 0) 
+                str += new string(' ', margin);
+            str += GetLining(LineType.IdxVertical | detail);
+            if ((dir & LineType.ConR) != 0) 
+                str += new string(' ', margin);
+            return str;
+        }
+
+        private string HoriDetailLine(int len, int[] trims, LineType diff, LineType detail = LineType.None)
+        {
+            var line = LineType.ConR;
+            var str = string.Empty;
+            int ti = 0, tc = -1;
+            for (var i = 0; i <= len; i++)
+            {
+                if (i == 1)
+                    line |= LineType.ConL;
+                if (i == len)
+                    line &= ~LineType.ConR;
+
+                var useDiff = false;
+                // trim counter
+                if (--tc <= 0 && ti < trims.Length)
+                {
+                    useDiff = true;
+                    tc = trims[ti++];
+                }
+
+                str += GetLining(line | detail | (useDiff ? diff : 0));
+            }
+
+            return str + '\n';
+        }
+
+        public enum LineMode
+        {
+            ASCII,
+            Unicode
+        }
+
+        private static readonly Dictionary<LineMode, Dictionary<LineType, char>> LiningSymbols = new Dictionary<LineMode, Dictionary<LineType, char>>
+        {
+            {
+                LineMode.ASCII,
+                new Dictionary<LineType, char>
+                {
+                    {LineType.IdxHorizontal, '-' },
+                    {LineType.IdxVertical, '|'},
+                    {LineType.IdxCrossing, '+'}
+                }
+            },
+            {
+                LineMode.Unicode,
+                new Dictionary<LineType, char>
+                {
+                    {LineType.IdxHorizontal, '─'},
+                    {LineType.IdxVertical, '│'},
+                    {LineType.IdxCrossing, '┼'},
+                    {LineType.IdxTHoriU, '┴'},
+                    {LineType.IdxTHoriD, '┬'},
+                    {LineType.IdxTVertL, '┤'},
+                    {LineType.IdxTVertR, '├'},
+                    {LineType.IdxCornerTL, '┌'},
+                    {LineType.IdxCornerTR, '┐'},
+                    {LineType.IdxCornerBL, '└'},
+                    {LineType.IdxCornerBR, '┘'},
+                    {LineType.Bold | LineType.IdxHorizontal, '═'},
+                    {LineType.Bold | LineType.IdxVertical, '║'},
+                    {LineType.Bold | LineType.IdxCrossing, '╬'},
+                    {LineType.Bold | LineType.IdxTHoriU, '╩'},
+                    {LineType.Bold | LineType.IdxTHoriD, '╦'},
+                    {LineType.Bold | LineType.IdxTVertL, '╣'},
+                    {LineType.Bold | LineType.IdxTVertR, '╠'},
+                    {LineType.Bold | LineType.IdxCornerTL, '╔'},
+                    {LineType.Bold | LineType.IdxCornerTR, '╗'},
+                    {LineType.Bold | LineType.IdxCornerBL, '╚'},
+                    {LineType.Bold | LineType.IdxCornerBR, '╝'}
+                }
+            }
+        };
+
+        [Flags]
+        private enum LineType : int
+        {
+            None = 0b0_0000,
+            Bold = 0b1_0000,
+            
+            ConU = 0b0_0001,
+            ConD = 0b0_0010,
+            ConL = 0b0_0100,
+            ConR = 0b0_1000,
+
+            IdxHorizontal = ConL | ConR,
+            IdxVertical = ConU | ConD,
+
+            IdxCrossing = IdxHorizontal | IdxVertical,
+            IdxTHoriU = IdxHorizontal | ConU,
+            IdxTHoriD = IdxHorizontal | ConD,
+            IdxTVertL = IdxVertical | ConL,
+            IdxTVertR = IdxVertical | ConR,
+            
+            IdxCornerTL = ConD | ConR,
+            IdxCornerTR = ConD | ConL,
+            IdxCornerBL = ConU | ConR,
+            IdxCornerBR = ConU | ConL
+        }
+
+        #endregion
+    }
+}
