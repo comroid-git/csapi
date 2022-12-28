17ac6f56528c59f3087c7e78e97817ea01589687
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-12-28 21:03:00 +0100

    Create DebugUtil.cs

---
+++ common/DebugUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace comroid.csapi.common
+{
+    public static class DebugUtil
+    {
+        public static Action<T> WithExceptionHandler<T>(Action<Exception> handler, Action<T> action) => it =>
+        {
+            try
+            {
+                action(it);
+            }
+            catch (Exception e)
+            {
+                handler(e);
+            }
+        };
+
+        public static Func<TIn, TOut?> WithExceptionHandler<TIn, TOut>(Action<Exception> handler,
+            Func<TIn, TOut> action) where TOut : class => WithExceptionHandler<TIn, TOut?>(e =>
+        {
+            handler(e);
+            return null;
+        }, action);
+
+        public static Func<TIn, TOut> WithExceptionHandler<TIn, TOut>(Func<Exception, TOut> handler,
+            Func<TIn, TOut> action) => it =>
+        {
+            try
+            {
+                return action(it);
+            }
+            catch (Exception e)
+            {
+                return handler(e);
+            }
+        };
+
+        public static Version GetAssemblyVersion<T>() => typeof(T).Assembly.GetName().Version;
+    }
+}
