8e9d0be685a1532c16b83e493396b9a00630be6e
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-01-08 20:21:01 +0100

    Create Container.cs

---
+++ common/Container.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace comroid.csapi.common;
+
+public class Container : HashSet<IDisposable>, IDisposable
+{
+    public void Dispose()
+    {
+        foreach (var disposable in this) 
+            disposable.Dispose();
+    }
+}
