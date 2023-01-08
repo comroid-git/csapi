c3ced1fd067889ad59ed2b79262657a43b42f9b8
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-01-08 20:21:04 +0100

    Create Extensions.cs

---
+++ common/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace comroid.csapi.common;
+
+public static class Extensions
+{
+    public static int Redirect(this Stream input, Stream output, int bufferSize = 64)
+    {
+        int r, c = 0;
+        var buf = new byte[bufferSize];
+        while ((r = input.Read(buf, 0, bufferSize)) != -1)
+        {
+            output.Write(buf, 0, r);
+            c += r;
+        }
+        return c;
+    }
+    
+    public static string CreateMd5ForFolder(this DirectoryInfo dir)
+    {
+        // https://stackoverflow.com/questions/3625658/how-do-you-create-the-hash-of-a-folder-in-c
+        // assuming you want to include nested folders
+        var files = Directory.GetFiles(dir.FullName, "*.*", SearchOption.AllDirectories)
+            .OrderBy(p => p).ToList();
+
+        var md5 = MD5.Create();
+
+        for(int i = 0; i < files.Count; i++)
+        {
+            var file = files[i];
+
+            // hash path
+            var relativePath = file.Substring(dir.FullName.Length + 1);
+            var pathBytes = Encoding.UTF8.GetBytes(relativePath.ToLower());
+            md5.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+            // hash contents
+            var contentBytes = File.ReadAllBytes(file);
+            if (i == files.Count - 1)
+                md5.TransformFinalBlock(contentBytes, 0, contentBytes.Length);
+            else md5.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+        }
+
+        return BitConverter.ToString(md5.Hash).Replace("-", "").ToLower();
+    }
+}
