e9eca3db88ccc24311af85b4db4b34bc7ef9abe5
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-01-16 17:54:08 +0100

    add testapp

---
+++ testapp/Program.cs
@@ -0,0 +1,16 @@
+﻿using common.test;
+
+namespace testapp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var test = new ByteAdapterTest();
+        
+        test.Setup();
+        test._1_SaveObj();
+        test._2_LoadObj();
+        test._3_LoadObj_Direct();
+    }
+}
