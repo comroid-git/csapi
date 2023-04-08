a521fbe767c6a5b449824b9e063f736f9e7a928b
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-04-08 17:11:00 +0200

    Update TestUnits.cs

--- common.test/TestUnits.cs
+++ common.test/TestUnits.cs
@@ -10,18 +10,28 @@ public class TestUnits
     {
         var input = Units.Parse("8kV");
         var output = input | SiPrefix.One;
-        
+
         Assert.That((double)output, Is.EqualTo(8000));
         Assert.That(output.ToString(), Is.EqualTo("8000V"));
     }
-    
+
     [Test]
     public void TestByteConversion()
     {
         var input = Units.Parse("8kB");
         var output = input | SiPrefix.One;
-        
+
         Assert.That((double)output, Is.EqualTo(8192));
         Assert.That(output.ToString(), Is.EqualTo("8192B"));
+    }
+
+    [Test]
+    public void TestFactorUnit()
+    {
+        var input = 2 * Units.Days;
+        var output = input * 24;
+
+        Assert.That((double)output, Is.EqualTo(2 * 24));
+        Assert.That(output.ToString(), Is.EqualTo($"{2 * 24}h"));
     }
 }
