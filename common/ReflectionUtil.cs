9445c0c57608adaa61e4b352fc08ed66363f4ce5
burdoto <kaleidox@comroid.org>
2023-05-19 04:08:19 +0200

    Update ReflectionUtil.cs

--- common/ReflectionUtil.cs
+++ common/ReflectionUtil.cs
@@ -4,5 +4,6 @@ namespace comroid.common;
 
 public static class ReflectionUtil
 {
+    public static bool IsArrayOf<T>(this Array array) => array.GetType().IsArrayOf<T>();
     public static bool IsArrayOf<T>(this Type type) => type.IsArray && type.GetElementType()!.IsAssignableTo(typeof(T));
 }
