4aa5dce40af0ea138aeed1d4a03fd4452a349934
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-11-15 16:39:31 +0100

    Update BigotryFilter.cs

--- common/BigotryFilter.cs
+++ common/BigotryFilter.cs
@@ -11,19 +11,28 @@ public sealed class BigotryFilter
 
     static BigotryFilter()
     {
-        var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "comroid", "pronouns.txt");
-
-        string content;
-        if (File.Exists(path))
-            content = File.ReadAllText(path);
-        else
+        try
         {
-            Console.Write("Please enter your preferred pronouns: ");
-            content = Console.ReadLine() ?? throw new NullReferenceException("No input detected");
-            File.WriteAllText(path, content);
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "comroid", "pronouns.txt");
+
+            string content;
+            if (File.Exists(path))
+                content = File.ReadAllText(path);
+            else
+            {
+                Console.Write("Please enter your preferred pronouns: ");
+                content = Console.ReadLine() ?? throw new NullReferenceException("No input detected");
+                File.WriteAllText(path, content);
+            }
+            if (content == string.Empty)
+                throw new ArgumentException("Please enter your pronouns");
+            Pronouns = content.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+            Log<BigotryFilter>.Config("Found Pronouns: " + string.Join("/", Pronouns));
         }
-        Pronouns = content.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
-        Log<BigotryFilter>.Config("Found Pronouns: " + string.Join("/", Pronouns));
+        catch (Exception e)
+        {
+            Log<BigotryFilter>.Error("Could not obtain pronouns", e);
+        }
     }
 
     public static void Init()
